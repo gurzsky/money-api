af172cc95f937e6684d7fb4239d4847bf53acd2b
Carlos Eduardo Gurski <gurski.ce@gmail.com>
2021-02-16 10:40:55 -0300

    enabling jackson fail on unknown properties

--- src/main/java/com/money/api/resource/CategoryResource.java
+++ src/main/java/com/money/api/resource/CategoryResource.java
@@ -6,14 +6,12 @@ import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -47,7 +45,11 @@ public class CategoryResource {
 	}
 	
 	@GetMapping("/{codigo}")
-	public Category searchByCode(@PathVariable Long codigo) {
-		return categoryRepository.findOne(codigo);
+	public ResponseEntity<Category> searchByCode(@PathVariable Long codigo) {
+		Category category = categoryRepository.findOne(codigo);
+		if (category != null)
+			return ResponseEntity.ok().body(category);
+		
+		return ResponseEntity.noContent().build();
 	}
 }
