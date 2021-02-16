5912e38a36fe4d252ba17f9f8aa1b4cf10e65f30
Carlos Eduardo Gurski <gurski.ce@gmail.com>
2021-02-16 11:52:11 -0300

    customizing messages from bean validation

--- src/main/java/com/money/api/exceptionHandler/MoneyExceptionHandler.java
+++ src/main/java/com/money/api/exceptionHandler/MoneyExceptionHandler.java
@@ -1,5 +1,9 @@
 package com.money.api.exceptionHandler;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -7,6 +11,9 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -24,15 +31,40 @@ public class MoneyExceptionHandler extends ResponseEntityExceptionHandler {
 		String userMessage = messageSource.getMessage("invalid.message", null, LocaleContextHolder.getLocale());
 		String developerMessage = ex.getCause().toString();
 		
-		return handleExceptionInternal(ex, new Erro(userMessage, developerMessage), headers, HttpStatus.BAD_REQUEST, request);
+		List<Error> errors = Arrays.asList(new Error(userMessage, developerMessage));
+				
+		return handleExceptionInternal(ex, errors, headers, HttpStatus.BAD_REQUEST, request);
 	}
 	
-	public static class Erro {
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		
+		List<Error> errors = listErrors(ex.getBindingResult());
+	
+		return handleExceptionInternal(ex, errors, headers, HttpStatus.BAD_REQUEST, request);
+	}
+	
+	private List<Error> listErrors(BindingResult bindingResult) {
+		
+		List<Error> errors = new ArrayList<>();
+		
+		for (FieldError fieldError : bindingResult.getFieldErrors()) {
+
+			errors.add(new Error(
+					messageSource.getMessage(fieldError, LocaleContextHolder.getLocale()), 
+					fieldError.toString()));
+		}
+		
+		return errors;		
+	}
+	
+	public static class Error {
 		
 		private String userMessage;
 		private String developerMessage;		
 		
-		public Erro(String userMessage, String developerMessage) {
+		public Error(String userMessage, String developerMessage) {
 			this.userMessage = userMessage;
 			this.developerMessage = developerMessage;
 		}
@@ -45,5 +77,4 @@ public class MoneyExceptionHandler extends ResponseEntityExceptionHandler {
 			return developerMessage;
 		}
 	}
-
 }
