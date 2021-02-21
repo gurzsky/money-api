ff575eacc7181c69a08e1b88b781842068d114e9
Carlos Eduardo Gurski <gurski.ce@gmail.com>
2021-02-21 14:00:11 -0300

    Implementing delete method in Release Class

--- src/main/java/com/money/api/resource/ReleaseResource.java
+++ src/main/java/com/money/api/resource/ReleaseResource.java
@@ -12,12 +12,14 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.money.api.event.ResourceCreatedEvent;
@@ -80,4 +82,12 @@ public class ReleaseResource {
 		return ResponseEntity.badRequest().body(errors);
 		
 	}
+	
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Long codigo) {
+		
+		releaseRepository.delete(codigo);
+	}
+	
 }
